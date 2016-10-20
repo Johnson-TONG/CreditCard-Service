c59c0feede8d3e907393077924ddc7f06d4d43ff
Johnson-TONG <johnson.asp.net@hotmail.com>
2016-10-20 17:17:41 +1100

    add business rules object and interfact structure

---
+++ UltraLazy.Domain/BusinessRules/BusinessRules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraLazy.Domain.BusinessRules
+{
+    public interface IBusinessRule
+    {
+        bool Validate();
+    }
+}
---
+++ UltraLazy.Domain/BusinessRules/IsCardNumberValid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraLazy.Domain.BusinessRules
+{
+    /// <summary>
+    /// Performs a Mod-10/LUHN check on the passed number and returns true if the check passed
+    /// </summary>
+    /// <param name="cardNumber">A 16 digit card number</param>
+    /// <returns>true if the card number is valid, otherwise false</returns>
+    /// <remarks>
+    /// Refer here for MOD10 algorithm: https://en.wikipedia.org/wiki/Luhn_algorithm
+    /// </remarks>
+    public class IsCardNumberValid : IBusinessRule
+    {
+        public IsCardNumberValid(string cardNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool Validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
