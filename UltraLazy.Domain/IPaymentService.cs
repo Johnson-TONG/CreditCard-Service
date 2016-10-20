99ade889f48b15c7a3266bb88d1314c897bb63ef
Johnson-TONG <johnson.asp.net@hotmail.com>
2016-10-20 17:07:33 +1100

    add new project

---
+++ UltraLazy.Domain/IPaymentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraLazy.Domain
+{
+    public interface IPaymentService
+    {
+        /// <summary>
+        /// Returns the unique ID allocated to a user
+        /// </summary>
+        string WhatsYourId();
+
+        /// <summary>
+		/// Returns the GUID allocated to payment
+		/// </summary>
+		Guid MakePayment(CreditCard creditCard);
+    }
+}
