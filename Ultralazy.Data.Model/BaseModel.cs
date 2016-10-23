65afcb8d24a692c1d13d0c08312c143ad65b737f
Johnson-TONG <johnson.asp.net@hotmail.com>
2016-10-23 14:16:00 +1100

    Add Data.Model.BaseModel base on IModel

---
+++ Ultralazy.Data.Model/BaseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ServiceStack.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Ultralazy.Data.Model
+{
+    /// <summary>
+    /// Mark DataContract for class BaseModel & Id as Primarykey with autoincrement enabled
+    /// </summary>
+    [DataContract]
+    public abstract class BaseModel : IModel
+    {
+        [DataMember(Order = 1)]
+        [AutoIncrement]
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [IgnoreDataMember]
+        public int CreatedBy { get; set; }
+
+        [IgnoreDataMember]
+        public int ModifiedBy { get; set; }
+
+        [IgnoreDataMember]
+        public virtual DateTime CreatedDate { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime ModifiedDate { get; set; }
+    }
+}
