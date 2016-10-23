33a7c348b185e72f5fed0a362b4fe68da27a9937
Johnson-TONG <johnson.asp.net@hotmail.com>
2016-10-23 13:50:51 +1100

    Add new Data.Model project

---
+++ Ultralazy.Data.Model/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Ultralazy.Data.Model
+{
+    /// <summary>
+    /// Base Interface Id, CreatedBy, ModifiedBy, CreatedDate & ModifiedDate
+    /// </summary>
+    public interface IModel
+    {
+        int Id { get; set; }
+        int CreatedBy { get; set; }
+        int ModifiedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime ModifiedDate { get; set; }
+    }
+}
